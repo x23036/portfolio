fcf4af983fa61e412ff93d38a1390cc3aaf2dc5e
x23036 <x23036xx@aitech.ac.jp>
2024-07-29 04:39:52 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+$(function () {
+  
+    var navHeight = $(".header").outerHeight();
+  
+    $('a[href^="#"]').on("click", function () {
+      var href = $(this).attr("href");
+      var target = $(href == "#" || href == "" ? "html" : href);
+      var position = target.offset().top - navHeight;
+      $("html, body").animate({ scrollTop: position, }, 300, "swing");
+      return false;
+    });
+  
+    $("#js-page-top").on("click", function () {
+      $("body,html").animate({ scrollTop: 0, }, 300);
+      return false;
+    });
+  });
+  
